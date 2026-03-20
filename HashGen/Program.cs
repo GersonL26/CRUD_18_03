823e0952679455eca13e49ee41e0f46a6b1ba57e
GersonL26 <gerson.lopez@farsiman.com>
2026-03-20 10:57:23 -0600

    Eliminado de HashGen de prueba

--- HashGen/Program.cs
+++
@@ -1,3 +0,0 @@
-var newHash = BCrypt.Net.BCrypt.HashPassword("Admin123!", workFactor: 11);
-Console.WriteLine("New hash: " + newHash);
-Console.WriteLine("Verify Admin123! => " + BCrypt.Net.BCrypt.Verify("Admin123!", newHash));
